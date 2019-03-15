271b099794f969386e8f144068edd69075498b37
FioPlay <FioPlay@users.noreply.github.com>
2019-03-15 15:13:18 +0100

    Jo

--- BT/src/D2B.java
+++ BT/src/D2B.java
@@ -2,8 +2,34 @@
 public class D2B {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String B = "100101001010010101";
+		Bit2Dez(B);
+		
+	}
+	
+	public static int Bit2Dez(String BinärZ) {
+		
+		char[] BinärArray = BinärZ.toCharArray();
+		int[] BinärZArray = new int[BinärArray.length];
+		
+		for (int i = 0; i < BinärZArray.length; i++) {
+			BinärZArray[i] = BinärArray[BinärArray.length - i]; 
+		}
+		
+		int Dez = 0;
+		
+		for (int i = 0; i < BinärZArray.length; i++) {
+			if(BinärZArray[i] == 1) {
+				
+			}else if(BinärZArray[i] == 1) {
+				
+			}else {
+				
+			}
+		}
+		
+		return Dez;
 	}
 
 }
